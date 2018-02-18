e1135d5d9f9460e967bd5e9275ccba571f0d7e16
Thomas Pinkava <pinkavat@grinnell.edu>
2018-02-18 13:09:50 -0600

    halfway commit for alphabeta

--- driver.scm
+++ driver.scm
@@ -17,3 +17,15 @@
 (require "mancala-player.scm")
 (require "game.scm")
 (require "general.scm")
+
+(define mancala (make-mancala-game)) 
+
+(define mancala-player1-eval (best-mancala-eval #t))
+(define mancala-player2-eval (simple-mancala-eval #f))
+
+(define mancala-player1 
+  (make-cutoff-minimax-player mancala 3 mancala-player1-eval))
+(define mancala-player2 
+  (make-cutoff-minimax-player mancala 3 mancala-player2-eval)) 
+
+(game-play mancala mancala-player1 mancala-player2)
